19419e7ccf8a1c3781ab7fa3a8f5e7803da8390e
Axiom88 <axiom88guru@gmail.com>
2020-08-16 23:51:36 +1000

    route fix on prod

--- server.js
+++ server.js
@@ -39,11 +39,16 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 // Serve React
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 app.use(express.static(path.join(__dirname, "client", "build")));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 
 const port = process.env.PORT || 5000;
 
